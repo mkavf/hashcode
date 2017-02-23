2f2ef358a6f30dc792f6d9bda50059606687e24f
Mykola Kavf <mykola.kavf@perfectial.com>
2017-02-23 21:12:49 +0200

    initial algorithm

--- 2017/qualification/src/model/Solution.java
+++ 2017/qualification/src/model/Solution.java
@@ -2,4 +2,14 @@ package model;
 
 
 public class Solution {
+    public final Cache[] filledCaches;
+
+    public Solution(Cache[] filledCaches) {
+
+        this.filledCaches = filledCaches;
+    }
+
+    public int getUsedCaches() {
+        return 0;
+    }
 }
--- 2017/qualification/src/solution/ProblemSolver.java
+++ 2017/qualification/src/solution/ProblemSolver.java
@@ -1,12 +1,38 @@
 package solution;
 
 
-import model.InputData;
+import model.Cache;
+import model.Endpoint;
+import model.Pair;
 import model.Solution;
+
+import java.util.List;
+import java.util.TreeSet;
 
 public class ProblemSolver {
 
-    public Solution getSolution(InputData inputData){
-        return null;
+    public Solution getSolution(List<Endpoint> endpoints) {
+
+        TreeSet<Pair> crossJoin = new TreeSet<>();
+        Cache[] result = new Cache[0];
+
+        for (int endpoint = 0; endpoint < endpoints.size(); endpoint++) {
+            Endpoint endpointInstance = endpoints.get(endpoint);
+            for (int video = 0; video < endpointInstance.getRequestsToVideo().length; video++) {
+                int videoRequests = endpointInstance.getRequestsToVideo()[video];
+                for (int cache = 0; cache < endpointInstance.getCacheLatency().length; cache++) {
+                    int cacheLatency = endpointInstance.getCacheLatency()[cache];
+                    int value = (endpointInstance.getDataCenterLatency() - cacheLatency) * videoRequests;
+
+                    crossJoin.add(new Pair(endpoint, cache, video, value));
+                }
+            }
+        }
+
+        for(Pair p : crossJoin){
+            result[p.cache].addVideo(p.video);
+        }
+
+        return new Solution(result);
     }
 }

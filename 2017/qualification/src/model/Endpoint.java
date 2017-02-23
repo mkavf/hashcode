dcb4e126a6549ff1814447af78367d5ed00e9ab0
p.hamernyk <pavlo.hamernyk@perfectial.com>
2017-02-23 20:39:05 +0200

    commit

--- 2017/qualification/src/model/Endpoint.java
+++ 2017/qualification/src/model/Endpoint.java
@@ -2,4 +2,31 @@ package model;
 
 public class Endpoint {
 
+    private int[] requestsToVideo;
+    private int[] cacheLatency;
+    private int dataCenterLatency;
+
+    public int[] getRequestsToVideo() {
+        return requestsToVideo;
+    }
+
+    public void setRequestsToVideo(int[] requestsToVideo) {
+        this.requestsToVideo = requestsToVideo;
+    }
+
+    public int[] getCacheLatency() {
+        return cacheLatency;
+    }
+
+    public void setCacheLatency(int[] cacheLatency) {
+        this.cacheLatency = cacheLatency;
+    }
+
+    public int getDataCenterLatency() {
+        return dataCenterLatency;
+    }
+
+    public void setDataCenterLatency(int dataCenterLatency) {
+        this.dataCenterLatency = dataCenterLatency;
+    }
 }

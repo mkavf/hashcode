e7df9094303cc7a92b0eb95e249fc17c70a56ad6
p.hamernyk <pavlo.hamernyk@perfectial.com>
2017-02-23 20:32:29 +0200

    commit

--- 2017/qualification/src/model/Cache.java
+++ 2017/qualification/src/model/Cache.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cache {
 
     private int index;
+
+    private List<Video> videos = new ArrayList<>();
 
     public int getIndex() {
         return index;
@@ -10,5 +15,17 @@ public class Cache {
 
     public void setIndex(int index) {
         this.index = index;
+    }
+
+    public List<Video> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+    }
+
+    public int getSize() {
+        return GLOBAL.CACHE_SIZE - videos.stream().mapToInt(Video::getSize).sum();
     }
 }
---
+++ 2017/qualification/src/model/GLOBAL.java
@@ -0,0 +1,5 @@
+package model;
+
+public class GLOBAL {
+    public static int CACHE_SIZE;
+}
